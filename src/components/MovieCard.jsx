77ba9c94b5314f652974cf4767e0290a4b71d00d
simoneburrai <burrai.s98@gmail.com>
2025-05-12 19:06:07 +0200

    feat: completed card movies css, added link to movie page

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,7 +1,19 @@
+import { Link } from "react-router-dom"
+
 const MovieCard = ({ movie }) => {
+    const moviePath = `/movies/:${movie.id}`
+    console.log(movie.image)
     return <div className="movie-card">
-        <h3>{movie.title}</h3>
-        <p>{movie.abstract}</p>
+
+        <div className="image-container">
+            <img src={movie.image} alt={movie.title} />
+        </div>
+        <div className="movie-info">
+            <h3>{movie.title}</h3>
+            <h4>{movie.genre}</h4>
+            <p>{movie.abstract}</p>
+            <button><Link to={moviePath}>More info...</Link></button>
+        </div >
     </div>
 }
 export default MovieCard;
