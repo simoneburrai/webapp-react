875cd38f13e4dd63a73666dcb1d1127106d6359e
simoneburrai <burrai.s98@gmail.com>
2025-05-13 12:15:47 +0200

    feat: added form reviews, not active yet

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -23,27 +23,36 @@ const MovieDetail = () => {
 
     console.log()
 
-    return movie && <div className="movie-detail">
-        <div className="image-container">
-            <img src={movie.image} alt={movie.title} />
-        </div>
-        <div className="info-movie">
-            <h1>{movie.title}</h1>
-            <h3>{movie.genre}</h3>
-            <h4>{movie.director}</h4>
-            <p>{movie.abstract}</p>
-            <div>{movie.release_year}</div>
-            <div>REVIEWS VOTE: {Math.round(movie.average_vote)}</div>
-        </div>
-        {reviews && <div className="movie-reviews">
-            {reviews.map(review => {
-                return <div key={review.id} className="review">
-                    <h4>{review.name}</h4>
-                    <h5>VOTE: {review.vote}</h5>
-                    <p>{review.text}</p>
-                </div>
-            })}
+    return <div className="movie-page">
+        {movie && <div className="movie-detail">
+            <div className="image-container">
+                <img src={movie.image} alt={movie.title} />
+            </div>
+            <div className="info-movie">
+                <h1>{movie.title}</h1>
+                <h3>{movie.genre}</h3>
+                <h4>{movie.director}</h4>
+                <p>{movie.abstract}</p>
+                <div>{movie.release_year}</div>
+                <div>REVIEWS VOTE: {Math.round(movie.average_vote)}</div>
+            </div>
         </div>}
+        {reviews && <div className="movie-reviews">
+            <div className="reviews-container">
+                {reviews.map(review => {
+                    return <div key={review.id} className="review">
+                        <h4>{review.name}</h4>
+                        <h5>VOTE: {review.vote}</h5>
+                        <p>{review.text}</p>
+                    </div>
+                })}
+            </div>
+            <form className="reviews-form">
+                <input type="text" placeholder="Inserisci la tua recensione qui" />
+                <button>SEND</button>
+            </form>
+        </div>
+        }
     </div>
 }
 
